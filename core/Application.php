d4c6a4268abdc4f6603cc2c1d89e91e6672ad655
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-10-25 20:01:52 +0700

    Create features for views templating done

--- core/Application.php
+++ core/Application.php
@@ -4,10 +4,12 @@ namespace app\core;
 
 class Application
 {
+    public static string $ROOT_DIR;
     public Router $router;
     public Request $request;
-    public function __construct()
+    public function __construct($rootPath)
     {
+        self::$ROOT_DIR = $rootPath;
         $this->request = new Request();
         $this->router = new Router($this->request);
     }
@@ -15,6 +17,6 @@ class Application
     public function run()
     {
         //todo
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 }
