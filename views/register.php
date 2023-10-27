2da1df86bc8403eca8e28d473e9249ff3dc8cffd
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-10-27 14:40:26 +0700

    Update view : adjust the html form layout in register.php

--- views/register.php
+++ views/register.php
@@ -1,17 +1,32 @@
-<h1>Contact Page</h1>
+<h1>Create an Account</h1>
 
 <form method="post">
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Subject</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" name="subject">
+    <div class="row">
+        <div class="col">
+            <div class="mb-3">
+                <label for="firstname" class="form-label">Firstname</label>
+                <input type="text" class="form-control" id="firstname" name="firstname">
+            </div>
+
+        </div>
+        <div class="col">
+            <div class="mb-3">
+                <label for="lastname" class="form-label">Lastname</label>
+                <input type="text" class="form-control" id="lastname" name="lastname">
+            </div>
+        </div>
     </div>
     <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Email</label>
-        <input type="email" class="form-control" id="exampleInputPassword1" name="email">
+        <label for="Email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="Email" name="email">
     </div>
     <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Body</label>
-        <textarea class="form-control" id="exampleInputPassword1" name="body"></textarea>
+        <label for="Password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="Password" name="password">
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <div class="mb-3">
+        <label for="Confirm_Password" class="form-label">Confirm Password</label>
+        <input type="password" class="form-control" id="Confirm_Password" name="confirmPassword">
+    </div>
+    <button type="submit" class="btn btn-primary">Register</button>
 </form>
