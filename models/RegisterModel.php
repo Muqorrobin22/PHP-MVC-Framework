724222d7cca10233e65cf0fc2cbe0eb00ab5bbbf
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-10-28 15:47:21 +0700

    Implement Model and create Model Core

---
+++ models/RegisterModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\models;
+
+use app\core\Model;
+
+class RegisterModel extends Model
+{
+
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $password;
+    public string $confirmPassword;
+
+    public function __construct()
+    {
+    }
+
+    public function register()
+    {
+        echo "Creating new User";
+    }
+}
