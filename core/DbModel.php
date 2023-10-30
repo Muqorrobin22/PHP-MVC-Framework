6d67d76c843f256f8e78b945c5f9c053f1d9b54a
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-10-30 16:25:23 +0700

    Implement hashing password to database

--- core/DbModel.php
+++ core/DbModel.php
@@ -7,7 +7,7 @@ abstract class DbModel extends Model
     abstract public function tableName() : string;
     abstract public function attributes() : array;
 
-    public function save() : bool
+    public function save()
     {
         $tableName = $this->tableName();
         $attributes = $this->attributes();
