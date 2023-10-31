8c5b275806a66c6dda7e39a22cdb14e25adc4e34
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-10-31 13:10:44 +0700

    Adjust the label to more readable

--- core/Model.php
+++ core/Model.php
@@ -23,6 +23,10 @@ abstract class Model
 
     abstract public function rules() : array;
 
+    public function labels() : array {
+        return [];
+    }
+
     public array $errors = [];
     public function validate()
     {
