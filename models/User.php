e7d69ba5c0c49622ba0fdcec13edcff7eea06a4b
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-10-31 10:56:11 +0700

    Add status to track whenever user active, inactive or deleted

--- models/User.php
+++ models/User.php
@@ -8,9 +8,14 @@ use app\core\Model;
 class User extends DbModel
 {
 
+    const STATUS_INACTIVE = 0;
+    const STATUS_ACTIVE = 1;
+    const STATUS_DELETED = 2;
+
     public string $firstname = "";
     public string $lastname = "";
     public string $email = "";
+    public int $status = self::STATUS_INACTIVE;
     public string $password = "";
     public string $confirmPassword = "";
 
@@ -25,6 +30,7 @@ class User extends DbModel
 
     public function save()
     {
+        $this->status = self::STATUS_INACTIVE;
         $this->password = password_hash($this->password, PASSWORD_DEFAULT);
         return parent::save();
     }
@@ -43,6 +49,6 @@ class User extends DbModel
 
     public function attributes() : array
     {
-        return ["firstname", "lastname", "email", "password"];
+        return ["firstname", "lastname", "email", "password" , "status"];
     }
 }
